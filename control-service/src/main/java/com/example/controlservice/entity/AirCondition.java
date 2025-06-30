c522e56c3622c72ef90cb2b262d9ab599fc51faf
Kim Du Hyeon <83499405+DuHyeon2@users.noreply.github.com>
2025-07-01 00:12:11 +0900

    update control

--- control-service/src/main/java/com/example/controlservice/entity/AirCondition.java
+++ control-service/src/main/java/com/example/controlservice/entity/AirCondition.java
@@ -1,7 +1,24 @@
 package com.example.controlservice.entity;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "air_condition")
 public class AirCondition {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long airConditionId;
 
+    private String name;
+
+    private String status;
+
+    private String mode;
+
+    private Float temperature;
+
+    private LocalDateTime lastUpdated;
 }
