c1c8ffce90ef8e2a33a14a9daa9a41b124ec8a24
Kim Du Hyeon <83499405+DuHyeon2@users.noreply.github.com>
2025-07-04 15:02:33 +0900

    heatlh update

---
+++ temp-service/src/main/java/com/duhyeon/tempservice/controller/HealthController.java
@@ -0,0 +1,13 @@
+package com.duhyeon.tempservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck(){
+        return "ok2";
+    }
+}
